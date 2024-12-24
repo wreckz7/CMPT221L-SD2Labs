7d4a7bc46f60b0b14ff4b4f546a48b47c31edd3f
Awais Razaque <156938148+wreckz7@users.noreply.github.com>
2024-12-24 14:20:42 -0500

    Lab 6: Building a React App from Scratch: A Step-by-Step Guide - JUST src folder
    Introduction: React has brought revolution in web development by providing a powerful and efficient way to build interactive user interfaces. In this assignment, we’ll go step-by-step to create a React app from scratch. We’ll go through everything from setting up the development environment to creating and rendering components.
    
    Reference: Building a React App from Scratch.docx on OneDrive

---
+++ 6 ReactAppRazaque/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+ 
+const App = () => { 
+  return ( 
+    <div> 
+      <h1>Hello, It's my first React App!</h1> 
+       <h2>Happy Coding!!</h2> 
+    </div> 
+  ); 
+}; 
+ 
+export default App; 
